193f47493fc0eacb10d41ee94e38239c892784f5
mrroland194 <mrroland194@gmail.com>
2023-02-07 15:52:25 +0100

    hozza adtuk a DOM ot

---
+++ idex.js
@@ -0,0 +1,8 @@
+console.dir(document);
+console.log(document.head)
+console.log(document.all)
+document.all[8].textContent="Masik Cim"
+console.log(document.getElementById("main_title"))
+let item = document.getElementsByClassName("item")
+console.log(item)
+item[2].style.backgroundColor="grey"
