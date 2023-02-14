28baccb668f2aa5e3e100f7a9c337de99ed05ea6
mrroland194 <mrroland194@gmail.com>
2023-02-14 12:09:22 +0100

    hasznaltuk a filtereket

--- idex.js
+++ idex.js
@@ -6,3 +6,17 @@ console.log(document.getElementById("main_title"))
 let item = document.getElementsByClassName("item")
 console.log(item)
 item[2].style.backgroundColor="grey"
+let items = document.getElementsByClassName("item");
+console.log(items);
+for (let i = 0; i < items.length; i++) {
+  if (i % 2 == 1) {
+    items[i].style.backgroundColor = "lightgray"
+  }
+}
+console.log(document.getElementsByTagName("h2"));
+console.log(document.querySelector("#elso a"))
+let elsolink= document.querySelector("#elso p");
+elsolink.style.color="red";
+console.log(document.querySelectorAll("#list h2"))
+let lista=document.querySelectorAll(".title")
+lista[1].style.color="red"
